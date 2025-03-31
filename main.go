69466cadeec2ffff2a1ff816b5fd6de8b15549d7
steven-rivera <83838429+steven-rivera@users.noreply.github.com>
2025-03-31 08:53:09 -0700

    implemented exit command

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	stdin := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("$ ")
+		command, err := stdin.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		command = strings.TrimSuffix(command, "\n")
+
+		if strings.HasPrefix(command, "exit") {
+			split := strings.SplitAfterN(command, " ", 2)
+			exitCode, err := strconv.Atoi(split[1])
+			if err == nil {
+				os.Exit(exitCode)
+			}
+		}
+
+		fmt.Println(command + ": command not found")
+	}
 }
