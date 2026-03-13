305436850afc62fede72706169eb00709cfba8a7
steven-rivera <83838429+steven-rivera@users.noreply.github.com>
2026-03-13 10:23:29 -0700

    run scripts now supports -t flag for running tests

--- run.sh
+++ run.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-HISTFILE="history.txt" go run ./app/
+run_tests=false
+
+while getopts "t" opt; do
+	case "$opt" in
+	t) run_tests=true ;;
+	*)
+		echo "Usage: $0 [-t]"
+		exit 1
+		;;
+	esac
+done
+
+if $run_tests; then
+	go test -v ./bitbash
+else
+	HISTFILE="history.txt" go run ./bitbash
+fi
